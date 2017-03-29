4bce26f69bb886a9541923163a40f60a69f7d2cb
Panachai <panachai.ny@gmail.com>
2017-03-29 19:55:52 +0700

    wait teacher

--- src/chitchatjavaServer/chitchatjavaServer.java
+++ src/chitchatjavaServer/chitchatjavaServer.java
@@ -9,42 +9,10 @@ import java.net.Socket;
 
 public class chitchatjavaServer extends javax.swing.JFrame {
 
-    private ServerSocket ss;
-    private Socket s;
-    private BufferedReader in;  //character
-    private PrintWriter out;    //character
-    private String msg;
+    private Connect ct = new Connect();
 
     public chitchatjavaServer() {
         initComponents();
-    }
-
-    public void connect() {
-        try {
-            //step 1 open port
-            ss = new ServerSocket(8888);
-
-            while (true) {
-                //step 2 wait for connect from client
-                s = ss.accept();    //เหมือน wait()
-
-                //step 3 create input and output
-                in = new BufferedReader(new InputStreamReader(s.getInputStream()));  //byte to character to buffered
-                out = new PrintWriter(s.getOutputStream()); //byte to character
-
-                //step 4 process
-                msg = in.readLine();    //echo server ส่งอะไรมา ตอบอันนั้นกลับ
-                out.println(msg);       //echo server
-                out.flush();
-
-                //step 5 close
-                s.close();
-            }
-            //ss.close();
-
-        } catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
-        }
     }
 
     @SuppressWarnings("unchecked")
@@ -57,6 +25,7 @@ public class chitchatjavaServer extends javax.swing.JFrame {
         bt_stop = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tb_showstatus = new javax.swing.JTable();
+        lb_status = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -70,6 +39,11 @@ public class chitchatjavaServer extends javax.swing.JFrame {
         });
 
         bt_stop.setText("STOP");
+        bt_stop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt_stopActionPerformed(evt);
+            }
+        });
 
         tb_showstatus.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -81,26 +55,34 @@ public class chitchatjavaServer extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tb_showstatus);
 
+        lb_status.setText("STATUS");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(47, 47, 47)
-                .addComponent(bt_start)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(bt_stop)
-                .addGap(42, 42, 42))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(115, 115, 115)
-                        .addComponent(jLabel1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(115, 115, 115)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(47, 47, 47)
+                                .addComponent(bt_start)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(tf_port, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(lb_status)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(bt_stop)
+                                .addGap(29, 29, 29))
+                            .addComponent(tf_port, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(13, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -113,18 +95,26 @@ public class chitchatjavaServer extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bt_start)
-                    .addComponent(bt_stop))
+                    .addComponent(bt_stop)
+                    .addComponent(lb_status))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void bt_startActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_startActionPerformed
-        connect();
+        ct.start();
+        lb_status.setText("running");
     }//GEN-LAST:event_bt_startActionPerformed
+
+    private void bt_stopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_stopActionPerformed
+        ct.interrupt();
+
+    }//GEN-LAST:event_bt_stopActionPerformed
 
     /**
      * @param args the command line arguments
@@ -161,11 +151,68 @@ public class chitchatjavaServer extends javax.swing.JFrame {
         });
     }
 
+    class Connect extends Thread {
+
+        private ServerSocket ss;
+        private Socket s;
+        private BufferedReader in;  //character
+        private PrintWriter out;    //character
+        private String msg;
+
+        public void run() {
+
+            int port = Integer.parseInt(tf_port.getText());
+
+            try {
+                //step 1 open port
+                ss = new ServerSocket(port);
+                System.out.println("before while ");
+                while (true) {
+                    
+                    //step 2 wait for connect from client
+                    s = ss.accept();    //เหมือน wait()
+
+                    //step 3 create input and output
+                    in = new BufferedReader(new InputStreamReader(s.getInputStream()));  //byte to character to buffered
+                    out = new PrintWriter(s.getOutputStream()); //byte to character
+
+                    //step 4 process
+                    msg = in.readLine();    //echo server ส่งอะไรมา ตอบอันนั้นกลับ
+                    out.println(msg);       //echo server
+                    out.flush();
+                    sleep(1);
+
+                    System.out.println("test 2");
+
+                    //step 5 close
+                    s.close();
+
+                }
+                //ss.close();
+
+            } catch (IOException ioe) {
+                System.out.println(ioe.getMessage());
+            } catch (InterruptedException ie) {
+                lb_status.setText("stop");
+                try {
+                    out.flush();
+                    s.close();
+                } catch (IOException ioe) {
+                    System.out.println(ioe.getMessage());
+                }
+            }
+
+        }
+
+    }
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bt_start;
     private javax.swing.JButton bt_stop;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lb_status;
     private javax.swing.JTable tb_showstatus;
     private javax.swing.JTextField tf_port;
     // End of variables declaration//GEN-END:variables
